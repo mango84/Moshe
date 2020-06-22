491ac2557ef08d36f979e6975fcfd948bf118710
Mangoli, Moshe <Moshe.Mangoli@ncr.com>
2020-06-22 21:58:49 +0300

    edit script

--- Scripts/script.ps1
+++ Scripts/script.ps1
@@ -1,6 +1,6 @@
 
-                            $R = $ENV:BUILD_NUMBER / 3
-                              if ($R.GetType().Name -eq "Double") {
+                            $BN = $ENV:BUILD_NUMBER / 3
+                              if ($BN.GetType().Name -eq "Double") {
                               throw "Script Failed - job name is: $ENV:JOB_NAME"
                               } else {
                                 "job name is: $ENV:JOB_NAME"
